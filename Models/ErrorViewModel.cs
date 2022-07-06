1e5fd981b29822d609936be59cbad802911eac45
Tculls <tanner.culley@gmail.com>
2022-07-06 14:04:37 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GolfScorePoster.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
